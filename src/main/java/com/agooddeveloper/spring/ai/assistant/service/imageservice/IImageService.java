e712d3e215e431b50982ec119f00fbbb83b931b2
thisismemukul <w41k3r.12@gmail.com>
2024-10-12 16:49:24 +0530

    IImageService

--- src/main/java/com/agooddeveloper/spring/ai/assistant/service/imageservice/IImageService.java
+++ src/main/java/com/agooddeveloper/spring/ai/assistant/service/imageservice/IImageService.java
@@ -1,6 +1,7 @@
 package com.agooddeveloper.spring.ai.assistant.service.imageservice;
 
 import com.agooddeveloper.spring.ai.assistant.response.diet.DietPlanResponse;
+import com.agooddeveloper.spring.ai.assistant.response.exercise.ExercisePlanResponse;
 import com.agooddeveloper.spring.ai.assistant.response.image.AiImageResponse;
 import com.agooddeveloper.spring.ai.assistant.response.image.TitleImage;
 import com.agooddeveloper.spring.ai.assistant.response.recipe.RecipeResponse;
@@ -11,4 +12,5 @@ import java.util.concurrent.CompletableFuture;
 public interface IImageService {
     CompletableFuture<AiImageResponse> generateRecipeImages(RecipeResponse recipeResponse);
     CompletableFuture<List<TitleImage>> generateDietImages(DietPlanResponse dietPlanResponse);
+    CompletableFuture<AiImageResponse> generateExerciseImages(ExercisePlanResponse exercisePlanResponse);
 }
