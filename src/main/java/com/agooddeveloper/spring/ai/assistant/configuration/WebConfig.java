6c0748945ed6b2963ab681130a4716a9f0f97581
thisismemukul <w41k3r.12@gmail.com>
2024-10-12 20:48:55 +0530

    WebConfig

---
+++ src/main/java/com/agooddeveloper/spring/ai/assistant/configuration/WebConfig.java
@@ -0,0 +1,25 @@
+package com.agooddeveloper.spring.ai.assistant.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.lang.NonNull;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(@NonNull CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://127.0.0.1:3000/")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
